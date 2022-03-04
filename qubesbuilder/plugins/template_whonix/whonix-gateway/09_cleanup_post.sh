ff945fc9a8312ceb7195fe4daa0e2c9e54af8a18
Frédéric Pierret (fepitre) <frederic.pierret@qubes-os.org>
2022-03-04 17:01:35 +0100

    Update scripts header licence
    This is done partially. We need to create an automatic licence update
    process in order to bump latest year every year.

--- qubesbuilder/plugins/template_whonix/whonix-gateway/09_cleanup_post.sh
+++ qubesbuilder/plugins/template_whonix/whonix-gateway/09_cleanup_post.sh
@@ -1,8 +1,28 @@
 #!/bin/bash -e
 # vim: set ts=4 sw=4 sts=4 et :
 
-## Copyright (C) 2012 - 2021 ENCRYPTED SUPPORT LP <adrelanos@whonix.org>
-## See the file COPYING for copying conditions.
+#
+# The Qubes OS Project, http://www.qubes-os.org
+#
+# Copyright (C) 2015-2021 Patrick Schleizer <adrelanos@whonix.org>
+# Copyright (C) 2015 Jason Mehring <nrgaway@gmail.com>
+# Copyright (C) 2022 Frédéric Pierret <frederic@invisiblethingslab.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program. If not, see <https://www.gnu.org/licenses/>.
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
 
 if [ "$VERBOSE" -ge 2 -o "$DEBUG" == "1" ]; then
     set -x
