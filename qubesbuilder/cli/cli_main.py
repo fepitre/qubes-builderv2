a8c01659743d5fce1954813113806f3bb4b32ce2
Frédéric Pierret (fepitre) <frederic.pierret@qubes-os.org>
2022-09-29 14:31:56 +0200

    Add support for passing all config values from CLI

--- qubesbuilder/cli/cli_main.py
+++ qubesbuilder/cli/cli_main.py
@@ -24,7 +24,7 @@ QubesBuilder command-line interface.
 from datetime import datetime
 from pathlib import Path
 from typing import List
-
+import re
 import click
 
 from qubesbuilder.cli.cli_base import ContextObj, aliased_group
@@ -35,37 +35,76 @@ from qubesbuilder.cli.cli_package import package
 from qubesbuilder.cli.cli_repository import repository
 from qubesbuilder.cli.cli_template import template
 from qubesbuilder.common import STAGES
-from qubesbuilder.config import Config
+from qubesbuilder.config import Config, deep_merge
 from qubesbuilder.log import get_logger, init_logging
 
 log = get_logger("cli")
 
+ALLOWED_KEY_PATTERN = r"[A-Za-z0-9]+"
+
+
+def parse_dict_from_array(array):
+    if not array:
+        raise ValueError("Empty array found!")
+    if not re.match(ALLOWED_KEY_PATTERN, array[0]):
+        raise ValueError(f"Invalid key found: '{array[0]}'.")
+    if len(array) > 1:
+        result = {array[0]: parse_dict_from_array(array[1:])}
+    else:
+        if "=" in array[0]:
+            key, value = array[0].split("=")
+            if not re.match(ALLOWED_KEY_PATTERN, key):
+                raise ValueError(f"Invalid key found: '{array[0]}'.")
+            result = {key: value}
+        else:
+            result = {array[0]: True}
+    return result
+
+
+def parse_config_entry_from_array(array):
+    result = {}
+    for s in array:
+        result = deep_merge(result, parse_dict_from_array(s.split(":")))
+    return result
+
 
 def init_context_obj(
-    builder_conf,
-    artifacts_dir: str = None,
+    builder_conf: str,
     verbose: int = None,
     debug: bool = None,
     log_file: str = None,
     component: List = None,
     distribution: List = None,
     template: List = None,
-    executor: str = None,
-    executor_option: List = None,
+    option: List = None,
 ):
-    config = Config(builder_conf)
-    if executor:
-        executor_config = {"type": executor, "options": {}}
-        for raw_option in executor_option or []:
-            parsed_option = raw_option.split("=", 1)
-            if len(parsed_option) != 2:
-                raise CliError("Invalid executor option.")
-            option, value = parsed_option
-            executor_config["options"][option] = str(value)  # type: ignore
-        config.set("executor", executor_config)
 
-    if artifacts_dir is not None:
-        config.set_artifacts_dir(Path(artifacts_dir).resolve())
+    try:
+        options = parse_config_entry_from_array(option) if option else {}
+    except ValueError as e:
+        raise CliError(f"Failed to parse CLI options: '{str(e)}'")
+
+    config = Config(builder_conf)
+
+    for opt in [
+        "git",
+        "executor",
+        "force-fetch",
+        "skip-git-fetch",
+        "fetch-versions-only",
+        "backend-vmm",
+        "use-qubes-repo",
+        "gpg-client",
+        "sign-key",
+        "min-age-days",
+        "qubes-release",
+        "repository-publish",
+        "repository-upload-remote-host",
+        "template-root-size",
+        "template-root-with-partitions",
+        "iso",
+    ]:
+        config.set(opt, config.get(opt, options.get(opt)))
 
     obj = ContextObj(config)
 
@@ -106,11 +145,6 @@ def init_context_obj(
     help="Path to configuration file (default: builder.yml).",
 )
 @click.option(
-    "--artifacts-dir",
-    default=None,
-    help="Path to artifacts directory (default: ./artifacts).",
-)
-@click.option(
     "--log-file",
     default=None,
     help="Path to log file to be created.",
@@ -120,34 +154,28 @@ def init_context_obj(
     "-c",
     default=None,
     multiple=True,
-    help="Override component in configuration file (can be repeated).",
+    help="Specify component to treat (can be repeated).",
 )
 @click.option(
     "--distribution",
     "-d",
     default=None,
     multiple=True,
-    help="Override distribution in configuration file (can be repeated).",
+    help="Set distribution to treat (can be repeated).",
 )
 @click.option(
     "--template",
     "-t",
     default=None,
     multiple=True,
-    help="Override template in configuration file (can be repeated).",
+    help="Set template to treat (can be repeated).",
 )
 @click.option(
-    "--executor",
-    "-e",
-    default=None,
-    help="Override executor type in configuration file.",
-)
-@click.option(
-    "--executor-option",
+    "--option",
+    "-o",
     default=None,
     multiple=True,
-    help='Override executor options in configuration file provided as "option=value" (can be repeated). '
-    'For example, --executor-option image="qubes-builder-fedora:latest"',
+    help="Set builder configuration value (can be repeated).",
 )
 @click.pass_context
 def main(
@@ -155,13 +183,11 @@ def main(
     verbose: int,
     debug: bool,
     builder_conf: str,
-    artifacts_dir: str = None,
-    log_file: str = None,
-    component: List = None,
-    distribution: List = None,
-    template: List = None,
-    executor: str = None,
-    executor_option: List = None,
+    log_file: str,
+    component: List,
+    distribution: List,
+    template: List,
+    option: List,
 ):
     """
     Main CLI
@@ -169,15 +195,13 @@ def main(
     """
     obj = init_context_obj(
         builder_conf=builder_conf,
-        artifacts_dir=artifacts_dir,
         verbose=verbose,
         debug=debug,
         log_file=log_file,
         component=component,
         distribution=distribution,
         template=template,
-        executor=executor,
-        executor_option=executor_option,
+        option=option,
     )
 
     if obj.config.verbose:
