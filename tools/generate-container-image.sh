b2c38ebcc3b552b53c1483bf6f1a38f9fb923fdd
Frédéric Pierret (fepitre) <frederic.pierret@qubes-os.org>
2022-10-15 10:17:53 +0200

    Improve container build process.
    - Allow to pull docker Fedora image
    - Update README

--- tools/generate-container-image.sh
+++ tools/generate-container-image.sh
@@ -2,14 +2,14 @@
 
 set -ex
 
-if [ $# -ne 2 ]; then
-    echo "Usage: $0 <CONTAINER_ENGINE> <MOCK_CONFIGURATION_FILE>" >&2
+if [ $# -lt 1 ]; then
+    echo "Usage: $0 CONTAINER_ENGINE <MOCK_CONFIGURATION_FILE>" >&2
+    echo "If MOCK_CONFIGURATION_FILE is provided, it will use Mock chroot as container rootfs." >&2
     exit 1
 fi
 
 CONTAINER_ENGINE="$1"
 MOCK_CONF="$2"
-MOCK_CONF_BN="$(basename "$MOCK_CONF")"
 
 [ -n "$CONTAINER_ENGINE" ] || {
     echo "Please provide container engine: 'docker' or 'podman'."
@@ -27,31 +27,34 @@ else
     CONTAINER_CMD="podman"
 fi
 
-[ -n "$MOCK_CONF" ] || {
-    echo "Please provide non empty mock configuration file."
-    exit 1
-}
-
 TOOLS_DIR="$(dirname "$0")"
 TOOLS_DIR="$(readlink -f "$TOOLS_DIR")"
 
-# Remove chroot and cache
-sudo mock \
-    -r "$MOCK_CONF" \
-    --scrub=all
+if [ -n "$MOCK_CONF" ]; then
+    MOCK_CONF_BN="$(basename "$MOCK_CONF")"
 
-# Create Mock chroot cache
-sudo mock \
-    -r "$MOCK_CONF" \
-    --init \
-    --no-bootstrap-chroot \
-    --config-opts chroot_setup_cmd='install dnf @buildsys-build'
+    # Remove chroot and cache
+    sudo mock \
+        -r "$MOCK_CONF" \
+        --scrub=all
 
-# Create Docker image
-# FIXME: The trim of .cfg extension does not work if rawhide is provided implicitly
-#  like at the time of writing 'fedora-37-x86_64'. We need to find a more reliable way
-#  to obtain mock chroot name.
-$CONTAINER_CMD build \
-    -f "${TOOLS_DIR}/../dockerfiles/fedora.Dockerfile" \
-    -t qubes-builder-fedora \
-    "/var/cache/mock/${MOCK_CONF_BN%.cfg}/root_cache/"
+    # Create Mock chroot cache
+    sudo mock \
+        -r "$MOCK_CONF" \
+        --init \
+        --no-bootstrap-chroot \
+        --config-opts chroot_setup_cmd='install dnf @buildsys-build'
+
+    # Create Docker image
+    # FIXME: The trim of .cfg extension does not work if rawhide is provided implicitly
+    #  like at the time of writing 'fedora-37-x86_64'. We need to find a more reliable way
+    #  to obtain mock chroot name.
+    $CONTAINER_CMD build \
+        -f "${TOOLS_DIR}/../dockerfiles/fedora-mock.Dockerfile" \
+        -t qubes-builder-fedora \
+        "/var/cache/mock/${MOCK_CONF_BN%.cfg}/root_cache/"
+else
+    $CONTAINER_CMD build \
+        -f "${TOOLS_DIR}/../dockerfiles/fedora.Dockerfile" \
+        -t qubes-builder-fedora .
+fi
