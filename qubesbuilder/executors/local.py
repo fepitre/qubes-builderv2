3e9f8f0655cbc2d412780f9212e4de008ff71685
Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>
2022-12-08 05:13:06 +0100

    local: do not fail on non-existing files to sed on
    The file list may be generic - replacements should be done for files
    that do exist.

--- qubesbuilder/executors/local.py
+++ qubesbuilder/executors/local.py
@@ -124,19 +124,17 @@ class LocalExecutor(Executor):
                 )
 
             # replace placeholders
-            sed_cmd = []
+            sed_cmd = ""
             if files_inside_executor_with_placeholders and isinstance(
                 files_inside_executor_with_placeholders, list
             ):
                 files = [str(f) for f in files_inside_executor_with_placeholders]
-                sed_cmd = [
-                    f"sed -i 's#@BUILDER_DIR@#{self.get_builder_dir()}#g' {' '.join(files)}"
-                ]
+                sed_cmd = f"sed -i 's#@BUILDER_DIR@#{self.get_builder_dir()}#g' {' '.join(files)};"
 
             final_cmd = [
                 "bash",
                 "-c",
-                "&&".join(sed_cmd + cmd),
+                sed_cmd + "&&".join(cmd),
             ]
             log.info(f"Executing '{' '.join(final_cmd)}'.")
 
