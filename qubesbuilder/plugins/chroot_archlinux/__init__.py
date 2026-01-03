fepitre/qubes-builderv2

commit 7cc4814993870e5f351ce7e4b9ed0642d4c2e10f

Refresh archlinux keys before building chroot
The keys packaged in distributions are not always the freshest one.
Refresh them from keyservers to get updated subkeys/signatures,
otherwise package verification fails, for example like this:

    [qb.chroot_archlinux.vm-archlinux.init-cache] error: libseccomp: signature from "Levente Polyak (anthraxx) <levente@leventepolyak.net>" is unknown trust
    [qb.chroot_archlinux.vm-archlinux.init-cache] error: failed to commit transaction (invalid or corrupted package)

qubesbuilder/plugins/chroot_archlinux/__init__.py
@@ -79,6 +79,8 @@ def get_archchroot_cmd(
         "sudo rm -rf /etc/pacman.d/gnupg/private-keys-v1.d",
         "sudo pacman-key --init",
         "sudo pacman-key --populate",
+        "sudo pacman-key --refresh-keys",
+        "sudo pacman-key --updatedb",
         f"sudo mkdir -p {chroot_dir.parent}",
         " ".join(mkarchchroot_cmd),
     ]

commit 85ca71f14f52c34eec98406c85ce33dee968264d

Do not fail init-cache if some keys cannot be refreshed
Keyservers are not reliable nowadays, especially over tor. Do key
refresh on best effort basis, but don't fail if some cannot be updated.
It's enough to get keys used for signing current packages (used in the
chroot).

Fixes: 7cc4814 "Refresh archlinux keys before building chroot"

qubesbuilder/plugins/chroot_archlinux/__init__.py
@@ -79,7 +79,7 @@ def get_archchroot_cmd(
         "sudo rm -rf /etc/pacman.d/gnupg/private-keys-v1.d",
         "sudo pacman-key --init",
         "sudo pacman-key --populate",
-        "sudo pacman-key --refresh-keys",
+        "(sudo pacman-key --refresh-keys || :)",
         "sudo pacman-key --updatedb",
         f"sudo mkdir -p {chroot_dir.parent}",
         " ".join(mkarchchroot_cmd),